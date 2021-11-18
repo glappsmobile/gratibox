18857d3eb8c4afcd257af90cc84ab07428de8014
Glauco Villas Boas <glaucovillasboas@pop-os.localdomain>
2021-11-18 13:49:02 -0300

    added a container to the home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Home = () => (
-  <div>
+  <Container>
     Home
-  </div>
+  </Container>
 );
+
+const Container = styled.main`
+  background-color: #6d7ce4;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 export default Home;

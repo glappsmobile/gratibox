97df43fb5da9673521c3808451628528f48fcbff
Glauco Villas Boas <glaucovillasboas@pop-os.localdomain>
2021-11-19 08:10:13 -0300

    getting user data from /user router on backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import GlobalStyle from './styles/GlobalStyle';
 import Home from './components/Home/Home';
@@ -6,9 +6,22 @@ import SignIn from './components/SignIn/SignIn';
 import SignUp from './components/SignUp/SignUp';
 import Plans from './components/Plans/Plans';
 import UserContext from './contexts/UserContext';
+import { getUser } from './services/gratibox.services';
 
 const App = () => {
   const [user, setUser] = useState();
+  useEffect(() => {
+    const token = JSON.parse(localStorage.getItem('token'));
+    if (token) {
+      getUser(token)
+        .then((response) => {
+          setUser({ ...response.data, token });
+        })
+        .catch(() => {
+          localStorage.removeItem('token');
+        });
+    }
+  }, []);
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
